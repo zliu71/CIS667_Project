67abd430c8ad6606cb4ef9e25b009f9c0b0db0eb
zliu71 <73616949+zliu71@users.noreply.github.com>
2020-12-09 16:42:38 -0600

    nn_network_a*_rubiks

---
+++ 2x2x2_nn_rubiks/generator.py
@@ -0,0 +1,110 @@
+import numpy as np
+import itertools as it
+import torch as tr
+import one_hot as oh
+import random
+
+class Node(object):
+    def __init__(self, state, depth = 1, step = 1, score = 0, path = []):
+        self.state = state
+        self.depth = depth
+        self.step = step
+        self.score = score
+        self.path = path
+        self.child_list = None
+        _, mtch = oh.cubie_match(state)
+        self.match = mtch
+        
+    def children(self):
+        if self.child_list is None: self.make_childrens()
+        return self.child_list
+    
+    def make_childrens(self):
+        self.child_list = list()
+        h = oh.heuristic(self.state, self.score, self.depth, self.match, self.path)
+        for heu in h:
+            self.child_list.append(Node(heu[0], self.depth, (self.step - 1), heu[1], heu[4]))
+        return self.child_list
+        
+def scramble(scramble_depth):
+    state = oh.initial_state()
+    for i in range(scramble_depth):
+        state = random.choice([state.rotx(), state.rot2x(), state.roty(), state.rot2y(), state.rotz(), state.rot2z()])
+        
+    return state;
+
+
+def make_choice(node):
+    minScore = 100000
+    minIndex = 0
+
+    bestRank = 0
+    rankIndex = 0
+    #print("length of list: ", len(node.children()))
+    for x in range (len(node.children())):
+        #print (node.children()[x].path)
+        
+        if bestRank < node.children()[x].match:
+            bestRank = node.children()[x].match
+            rankIndex = x
+        if bestRank == node.children()[x].match:
+            if node.children()[rankIndex].score > node.children()[x].score:
+                rankIndex = x
+                #print(node.children()[x].path)
+            
+    return rankIndex
+
+def generate(scrambe_depth = 5, depth = 1, steps = 1, num_games = 1):
+    data = list()
+    for game in range(num_games):
+        state = scramble(scramble_depth)
+        node = Node(state, depth, steps)
+        #print("\nScramble")
+        #node.state.disp()
+        for step in range(steps):
+            print("game %d, turn %d"%(game,step))
+            
+            #generate states
+            #node.children()
+            
+            #add states to data ##
+            for child in range(len(node.children())):
+                data.append(((node.children()[child].state), (node.children()[child].match)))
+            #make choice, the most matched and lowest score one
+            i = make_choice(node)
+            node = node.children()[i]
+            #if solved, ended
+            if node.match == 8:
+                print("\nSolve")
+                print("Path is :")
+                print(node.path)
+                #node.state.disp()
+                break
+            #print("\nCurrent best resut is:")
+            #node.state.disp()
+
+    return data
+            
+def get_batch(scramble_depth, depth, steps, num_games):
+    data = generate(scramble_depth, depth, steps, num_games)
+    length = len(data)
+    inputs = tr.zeros([length,2,2,2,3,6])
+    outputs = tr.zeros([length, 1])
+
+    for i in range(length):
+        a, b = data[i]
+        inputs[i] = tr.from_numpy(a.cube)
+        outputs[i, 0] = b
+
+    return inputs, outputs
+
+if __name__ == "__main__":
+    print("\nPlease input the size of the training data: ")
+    scramble_depth = int(input("\nPlease input the scramble_depth: "))
+    depth = int(input("\nPlease input the max depth of each step: "))
+    steps = int(input("\nPlease input the max steps in a game: "))
+    num_games = int(input("\nPlease input the number of the games: "))
+    inputs, outputs = get_batch(scramble_depth, depth, steps, num_games)
+    
+    import pickle as pk
+    with open("training_data_%d_scramble_depth.pkl" % scramble_depth, "wb") as f: pk.dump((inputs, outputs), f) 
---
+++ 2x2x2_nn_rubiks/neuron_network.py
@@ -0,0 +1,76 @@
+import numpy as np
+import torch as tr
+from torch.nn import Sequential, Conv2d, Linear, Flatten, LeakyReLU, Tanh
+
+def RubikNet():
+    return tr.nn.Sequential(tr.nn.Flatten(), tr.nn.Linear((2*2*2*3*6),1))
+
+def calculate_loss(net, x, y_targ):
+    size = x.size()
+    batch_size = size[0]
+    in_features = 1
+    for i in size:
+        if i != batch_size:
+            in_features = in_features * i
+    out_features = 1
+    """
+    e = 0
+    for i in range(len(x[:,0])):
+        weight_data = tr.randn(len(tr.flatten(x[i])),1)
+        bias_data = tr.randn(len(tr.flatten(y_targ[i])),1)
+        net.weight_data = weight_data
+        net.bias_data = bias_data
+        y = net(tr.flatten(x[i]))
+        e = e + (y - y_targ[i])**2
+    """
+    net.weight_data = tr.randn(in_features,1)
+    net.bias_data = tr.randn(out_features)
+    y = net(x.reshape(batch_size, in_features))
+    e = tr.sum((y - y_targ)**2)
+    return (y, e)
+
+def optimization_step(optimizer, net, x, y_targ):
+    optimizer.zero_grad()
+    y, e = calculate_loss(net, x, y_targ)
+    e.backward()
+    optimizer.step()
+    return (y, e)
+
+if __name__ == "__main__":
+
+    scramble_depth = int(input("\nPlease input the scramble_depth: "))
+    net = RubikNet()
+    print(net)
+
+    import pickle as pk
+    with open("training_data_%d_scramble_depth.pkl" % scramble_depth, "rb") as f: (x, y_targ) = pk.load(f)
+
+    # Optimization loop
+    optimizer = tr.optim.Adam(net.parameters())
+    train_loss, test_loss = [], []
+    shuffle = np.random.permutation(range(len(x)))
+    split = 10
+    train, test = shuffle[:-split], shuffle[-split:]
+    for epoch in range(200):
+        y_train, e_train = optimization_step(optimizer, net, x[train], y_targ[train])
+        y_test, e_test = calculate_loss(net, x[test], y_targ[test])
+        if epoch % 10 == 0: print("%d: %f (%f)" % (epoch, e_train.item(), e_test.item()))
+        train_loss.append(e_train.item() / (len(shuffle)-split))
+        test_loss.append(e_test.item() / split)
+    
+    tr.save(net.state_dict(), "model_%d.pth" % scramble_depth)
+    
+    import matplotlib.pyplot as pt
+    pt.plot(train_loss,'b-')
+    pt.plot(test_loss,'r-')
+    pt.legend(["Train","Test"])
+    pt.xlabel("Iteration")
+    pt.ylabel("Average Loss")
+    pt.show()
+    
+    pt.plot(y_train.detach().numpy(), y_targ[train].detach().numpy(),'bo')
+    pt.plot(y_test.detach().numpy(), y_targ[test].detach().numpy(),'ro')
+    pt.legend(["Train","Test"])
+    pt.xlabel("Actual output")
+    pt.ylabel("Target output")
+    pt.show()
